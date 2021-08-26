d9a4750526a4eafaa1012e6945b31c87ef391751
johni1228 <akirus1228@gmail.com>
2021-08-26 04:27:38 -0700

    fix: .png to jpg

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/api/v0/nfts/:nftId', function(req, res) {
         const jsonString = jsonBuffer.toString();
         const jsonData = JSON.parse(jsonString);
         jsonData.external_url = `https://example.com/?token_id=${nftId}`;
-        jsonData.image = `${imageURI}/images/polarbear${nftId}.png`;
+        jsonData.image = `${imageURI}/images/polarbear${nftId}.jpg`;
         return res.json(jsonData);
     } catch (e) {
         console.log(e);
