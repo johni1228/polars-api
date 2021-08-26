9d660d4cdd3ff71306d9e6b04e6ac200536fff76
johni1228 <akirus1228@gmail.com>
2021-08-26 02:24:20 -0700

    fex: public and update img and json url

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const fs = require("fs");
 const app = express();
 
 const port = parseInt(process.env.PORT || 3000);
-const dataDir = process.env.DATA_DIR || '/mnt/e/workspace/banguard/data';
+const dataDir = process.env.DATA_DIR || 'public';
 const imageURI = process.env.IMAGE_URI || 'http://127.0.0.1:3000';
 
 app.get('', function(req, res) {
-    res.json({ message: 'welcome to computer NFT api' });
+    res.json({ message: 'welcome to polar bears NFT api' });
 });
 
-app.get('/computer-api/v0/nfts/images/:image', function(req, res) {
+app.get('/images/:image', function(req, res) {
     const imageName = req.params.image;
-    const imagePath = `${dataDir}/images/${imageName}`;
+    const imagePath = `${__dirname}/${dataDir}/images/${imageName}`;
     console.log(imagePath);
     if (fs.existsSync(imagePath)) {
         return res.sendFile(imagePath);
@@ -21,15 +21,15 @@ app.get('/computer-api/v0/nfts/images/:image', function(req, res) {
     }
 });
 
-app.get('/computer-api/v0/nfts/:nftId', function(req, res) {
+app.get('/api/v0/nfts/:nftId', function(req, res) {
     const nftId = parseInt(req.params.nftId) + 1;
     const jsonPath = `${dataDir}/json/${nftId}.json`;
     try {
         const jsonBuffer = fs.readFileSync(jsonPath);
         const jsonString = jsonBuffer.toString();
         const jsonData = JSON.parse(jsonString);
-        jsonData.external_url = `https://example.com/?token_id=${nftId}`;
-        jsonData.image = `${imageURI}/computer-api/v0/nfts/images/${nftId}.png`;
+        jsonData.external_url = `${imageURI}/images/${nftId}.png`;
+        jsonData.image = `${imageURI}/images/${nftId}.png`;
         return res.json(jsonData);
     } catch (e) {
         console.log(e);
